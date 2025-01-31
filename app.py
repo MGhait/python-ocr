c2010c4f3b0a38e0d8597cb6acd0067b5655cd7d
Mohamed Ghait -U24 <mosaad05019@gmail.com>
2025-01-31 22:14:28 +0200

    for production

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import easyocr
 import cv2
@@ -42,4 +43,5 @@ def ocr():
         return jsonify({"error": f"OCR failed: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
